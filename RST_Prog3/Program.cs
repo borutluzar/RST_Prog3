797e58a88d65ceb6234349f4ca59042afe60d1ba
Borut Lužar <borut.luzar@gmail.com>
2026-03-19 10:26:26 +0100

    Dopolnjena koda za prvo srečanje.

--- RST_Prog3/Program.cs
+++ RST_Prog3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq.Expressions;
+using static RST_Prog3.ObjectsAndProperties;
 
 namespace RST_Prog3
 {
@@ -22,114 +23,8 @@ namespace RST_Prog3
 
             Console.WriteLine($"Imamo mizo z id-jem {miza1.ID}");
 
-            // Kreiramo še nov indeks:
-            Index index = new Index(20, StudyProgram.RST);
-            index.Year = 2;
-
-            Subject prog3 = new Subject(1) 
-            { 
-                Name = "Programiranje 3", 
-                Grade = 10, 
-                ECTS = 6, 
-                DateCompleted = new DateOnly(2026, 6, 4) 
-            };
-            Subject baze = new Subject(2)
-            {
-                Name = "Baze in modeliranje podatkov",
-                Grade = 10,
-                ECTS = 6,
-                DateCompleted = new DateOnly(2026, 6, 10)
-            };
-
-            index.Subjects.Add(prog3);
-            index.Subjects.Add(baze);
-
-            Console.WriteLine($"{index}");
             Console.Read();
         }
     }
 
-    public class Table
-    {
-        private int id; // Če je spremenljivka privatna, izven razreda ne moremo brati niti njene vrednosti
-        public string material;
-        public string color;
-        public Dimensions dimensions;
-
-        /// <summary>
-        /// Konstruktor, ki nastavi dani id.
-        /// </summary>
-        /// <param name="id">Zunaj pridobljeni id.</param>
-        public Table(int id)
-        {
-            this.id = id;
-        }
-
-        /// <summary>
-        /// Prazen konstruktor s privzeto vrednostjo id-ja.
-        /// </summary>
-        public Table()
-        {
-            this.id = 1;
-        }
-
-        /// <summary>
-        /// Lastnost (property), ki nam omogoči pridobivanje
-        /// vrednosti privatne spremenljivke id.
-        /// </summary>
-        public int ID
-        {
-            get
-            {
-                return this.id;
-            }
-            private set
-            {
-                this.id = value;
-            }
-        }
-
-        private double weight;
-        public double Weight
-        {
-            get
-            {
-                return weight;
-            }
-            set
-            {
-                if(value > 100 || value < 0)
-                {
-                    throw new ArgumentException("Teža ni v intervalu dovoljenih tež!");
-                }
-                this.weight = value;
-            }
-        }
-
-        /// <summary>
-        /// Samodejno implementirana lastnost (auto-implemented property)
-        /// </summary>
-        public double Price { get; set; }
-    }
-
-    /// <summary>
-    /// Vse tri dimenzije objekta.
-    /// </summary>
-    public struct Dimensions
-    {
-        /// <summary>
-        /// Višina mize
-        /// </summary>
-        public double Height { get; set; }
-        
-        /// <summary>
-        /// Širinia mize
-        /// </summary>
-        public double Width { get; set; }
-        
-        /// <summary>
-        /// Globina mize
-        /// </summary>
-        public double Depth { get; set; }
-    }
 }
